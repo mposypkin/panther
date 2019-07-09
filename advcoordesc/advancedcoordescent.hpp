999956d81b2281577f2136520c66f3f63a15339a
Mikhail Posypkin <mposypkin@gmail.com>
2019-07-09 14:32:23 +0300

    [add] Coordinate descent

--- advcoordesc/advancedcoordescent.hpp
+++ advcoordesc/advancedcoordescent.hpp
@@ -16,6 +16,7 @@
 
 #include <sstream>
 #include <vector>
+#include <algorithm>
 #include <functional>
 #include <memory>
 #include <common/bbsolver.hpp>
@@ -29,12 +30,54 @@ namespace panther {
     template <class T> class AdvancedCoorDescent : public BlackBoxSolver <T> {
     public:
 
+        struct Options {
+            // Initial step (positive)
+            T mInitStep = 1e-1;
+            // Incremental parameter
+            T mInc = 1.5;
+            // Decremental parameter
+            T mDec = 0.5;
+            // Minimal step size
+            T mMinStep = 1e-3;
+        } mOptions;
+
         T search(int n, T* x, const T * const a, const T * const b, const std::function<T(const T * const)> &f) override {
-            return 0;
+            std::vector<T> sft(n, mOptions.mInitStep);
+            auto maxStep = [&sft, n]() {
+                T rv = 0;
+                for (int i = 0; i < n; i++) {
+                    rv = std::max(rv, sft[i]);
+                }
+                return rv;
+            };
+            T v = f(x);
+            while (maxStep() >= mOptions.mMinStep) {
+                for (int i = 0; i < n; i++) {
+                    const T h = sft[i];
+                    const T xi = x[i];
+                    x[i] = std::min(x[i] + h, b[i]);
+                    T vn = f(x);
+                    if (vn < v) {
+                        v = vn;
+                        sft[i] *= mOptions.mInc;
+                    } else {
+                        x[i] = std::max(x[i] - 2 * h, a[i]);
+                        vn = f(x);
+                        if (vn < v) {
+                            v = vn;
+                            sft[i] *= mOptions.mInc;
+                        } else {
+                            x[i] = xi;
+                            sft[i] *= mOptions.mDec;
+                        }
+                    }
+                }
+            }
+            return v;
         }
 
     private:
-        
+
 
     };
 }
--- advcoordesc/testadvcd.cpp
+++ advcoordesc/testadvcd.cpp
@@ -10,15 +10,50 @@
  *
  * Created on June 28, 2019, 5:03 PM
  */
-
+#include <iostream>
+#include <iterator>
 #include "advancedcoordescent.hpp"
 
-using namespace std;
+constexpr int n = 3;
+
+//double f(const double* x) {
+//    double v;
+//    for (int i = 0; i < n; i++)
+//        v += x[i] * x[i];
+//    return v;
+//}
+
+struct F {
+
+    double operator()(const double *x) {
+        double v;
+        mCnt ++;
+        for (int i = 0; i < n; i++)
+            v += x[i] * x[i];
+        return v;
+    }
+    
+    int mCnt = 0;
+};
 
 /*
  * 
  */
 int main(int argc, char** argv) {
+
+    panther::AdvancedCoorDescent<double> adv;
+    double x[n];
+    double a[n], b[n];
+    std::fill(a, a + n, -1);
+    std::fill(b, b + n, 2);
+    std::fill(x, x + n, 1);
+    F f;
+    double v = adv.search(n, x, a, b, std::ref(f));
+    std::cout << "Found " << v << " at [";
+    std::copy(x, x + n, std::ostream_iterator<double>(std::cout, " "));
+    std::cout << "]\n";
+    std::cout << f.mCnt << " function calls done\n";
+
 
     return 0;
 }

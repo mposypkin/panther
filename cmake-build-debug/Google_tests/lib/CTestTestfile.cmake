968b37d978cacce8c4fcf326b5c6bdac3b292202
kate1mag <kate.magdetskaya@gmail.com>
2021-04-22 15:51:45 +0300

    Add CoordinateDescentMethod + тесты

---
+++ cmake-build-debug/Google_tests/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/user/Desktop/panther-master3/Google_tests/lib
+# Build directory: /Users/user/Desktop/panther-master3/cmake-build-debug/Google_tests/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
